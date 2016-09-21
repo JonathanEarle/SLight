a987c73cbdc2451661f4edea7a653cd7ef5aed26
Jonathan <jonathan.john.enock.earle@gmail.com>
2016-09-20 22:22:53 -0400

    Updated PitoPiCommunication

--- PitoPiCommunication/rec.py
+++ PitoPiCommunication/rec.py
@@ -1,13 +1,13 @@
 import socket
+import pickle
 
-host='192.168.1.6'
-port=5560
+host='192.168.1.2' #IP of host
+port=5560 #Port Number
+
 
 while True:
 	s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 	s.connect((host,port))
-
-	reply=s.recv(1024)
-	print(reply)
-
+	data=pickle.loads(s.recv(1024))
+	print(data)
 	s.close()
--- PitoPiCommunication/send.py
+++ PitoPiCommunication/send.py
@@ -1,10 +1,17 @@
 import socket
+import pickle
+import threading
 
 host=''
-port=5560
+#serverIP='192.168.1.2' #IP of server to connect to, ie Next Pi
+port=5560 #Port number
 
-msg=[1,2,3]
+#Data to be sent
+one=1
+two=2
+data={"one":one,"two":two}
 
+#Function to setup the server from which data will be retrieved
 def setupServer():
         s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         print("Socket created")
@@ -15,24 +22,39 @@ def setupServer():
         print("Socket bind complete")
         return s
 
+#Function which makes server waits for client to connect to it
 def setupConnection():
         s.listen(1)
         conn, addr=s.accept()
-        print("Connected to: "+addr[0]+" :"+ str(addr[1]))
+        #print("Connected to: "+addr[0]+" :"+ str(addr[1]))
         return conn
 
+#Function which sends the data to the client
 def dataTransfer(conn):
-#       data=conn.recv(1024)
-        reply=msg
-        conn.sendall(str(reply))
-        print("Data has been sent")
+        conn.sendall(pickle.dumps(data))
+        #print("Data has been sent")
 
-s=setupServer()
+#Code which makes program act as a client too
+#def Client():
+#	while True:
+#		s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+#		s.connect((serverIP,port))
+#		data=pickle.loads(s.recv(1024))
+#		print(data)
+#		s.close()
 
+s=setupServer() #Set up the server
+#threading.Thread(target=Client).start()	#Start the client thread
+
+#Continuously loop through waiting for client to connect to server, sending the data and then waiting for data to change
 while True:
-        try:
+        try:	
                 conn=setupConnection()
                 dataTransfer(conn)
+		
+		one=input("Enter one: ")
+		two=input("Enter two: ")
+		data={"one":one,"two":two}		
         except:
                 print('Error')
                 break
